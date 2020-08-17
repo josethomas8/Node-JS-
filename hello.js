3bfe7b1b98aca4e81c4c54a947b3c1df79cd8dc2
josethomas8 <joseeey8@gmail.com>
2020-08-17 18:56:33 +0530

    trying

---
+++ hello.js
@@ -0,0 +1,16 @@
+var name = "jose"
+
+var name1= new String ("Jose ")
+
+
+console.log(name1.length)
+
+
+k=1666
+
+
+var n ="10"
+var s=true
+console.log("Welcome "+name +  "     number  " +n)
+
+console.log(k)
